a424fdbf114a3eab151610cb16f5e6ce775d430a
Tobias Wallura <tobias@wallura.org>
2018-09-22 21:45:13 +0200

    use localized region to get example country in help intent

--- src/handlers/AMAZON_HelpIntentHandler.ts
+++ src/handlers/AMAZON_HelpIntentHandler.ts
@@ -1,5 +1,6 @@
 import { HandlerInput, RequestHandler } from "ask-sdk-core";
 import { Response } from "ask-sdk-model";
+import { Region } from "country-data";
 import { getRandomCountry } from "../utils";
 
 export class AmazonHelpIntentHandler implements RequestHandler {
@@ -10,13 +11,33 @@ export class AmazonHelpIntentHandler implements RequestHandler {
 
   public handle(handlerInput: HandlerInput): Response {
     const t = handlerInput.attributesManager.getRequestAttributes().t;
-    const locale = handlerInput.requestEnvelope.request.locale;
-    const country = getRandomCountry("Europa", locale);
+    const locale = handlerInput.requestEnvelope.request.locale as Locale;
+    const country = getRandomCountry(this.getRegion(locale), locale);
 
     return handlerInput.responseBuilder
       .speak(`${t("help.text", country.name)} ${t("help.reprompt")}`)
       .reprompt(t("help.reprompt"))
       .withShouldEndSession(false)
       .getResponse();
+  }
+
+  private getRegion(locale: Locale): Region {
+    switch (locale) {
+      case "en-US":
+      case "en-CA":
+        return Region.NORTH_AMERICA;
+      case "es-MX":
+        return Region.AMERICAS;
+      case "en-IN":
+        return Region.ASIA;
+      case "en-AU":
+        return Region.OCEANIA;
+      case "de-DE":
+      case "en-GB":
+      case "es-ES":
+      case "it-IT":
+      case "fr-FR":
+        return Region.EUROPE;
+    }
   }
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,3 +2,5 @@ declare module "*.json" {
   const value: any;
   export = value;
 }
+
+type Locale = "en-US" | "en-CA" | "en-IN" | "en-AU" | "en-GB" | "es-ES" | "es-MX" | "fr-FR" | "it-IT" | "de-DE";
