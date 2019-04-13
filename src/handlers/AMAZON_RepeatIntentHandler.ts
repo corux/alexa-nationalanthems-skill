9f55557e19a1784da65fef2f8370759d2ff0bdda
Tobias Wallura <tobias@wallura.org>
2019-04-13 15:19:03 +0200

    allow repeat with AudioPlayer interface

--- src/handlers/AMAZON_RepeatIntentHandler.ts
+++ src/handlers/AMAZON_RepeatIntentHandler.ts
@@ -1,7 +1,8 @@
+import { ICountry } from "@corux/country-data";
 import { HandlerInput } from "ask-sdk-core";
 import { Response } from "ask-sdk-model";
 import countries from "../data/countries";
-import { BaseIntentHandler, getAnthemUrl, getLocale, Intents } from "../utils";
+import { BaseIntentHandler, getAnthemUrl, getLocale, Intents, supportsAudioPlayer } from "../utils";
 
 @Intents("AMAZON.RepeatIntent", "AMAZON.StartOverIntent")
 export class AmazonRepeatIntentHandler extends BaseIntentHandler {
@@ -10,6 +11,17 @@ export class AmazonRepeatIntentHandler extends BaseIntentHandler {
     const session = handlerInput.attributesManager.getSessionAttributes();
     const t = handlerInput.attributesManager.getRequestAttributes().t;
     const locale = getLocale(handlerInput);
+
+    const countryFromAudioPlayer = this.getCountryFromAudioPlayer(handlerInput);
+    if (supportsAudioPlayer(handlerInput) && countryFromAudioPlayer) {
+      return responseBuilder
+        .addAudioPlayerPlayDirective("REPLACE_ALL", getAnthemUrl(countryFromAudioPlayer, true),
+          countryFromAudioPlayer.iso3, 0, undefined, {
+            title: `${countryFromAudioPlayer.name}: ${countryFromAudioPlayer.anthemName}`,
+          })
+        .withShouldEndSession(true)
+        .getResponse();
+    }
 
     if (session.iso) {
       const country = countries.getByIso3(session.iso, locale);
@@ -23,5 +35,15 @@ export class AmazonRepeatIntentHandler extends BaseIntentHandler {
       .speak(`${t("play.no-repeat")} ${t("play.reprompt")}`)
       .reprompt(t("play.reprompt"))
       .getResponse();
+  }
+
+  private getCountryFromAudioPlayer(handlerInput: HandlerInput): ICountry {
+    if (handlerInput.requestEnvelope.context.AudioPlayer) {
+      const locale = getLocale(handlerInput);
+      const country = countries.getByIso3(handlerInput.requestEnvelope.context.AudioPlayer.token, locale);
+      return country;
+    }
+
+    return undefined;
   }
 }
