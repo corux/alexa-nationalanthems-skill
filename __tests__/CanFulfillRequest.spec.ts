4fae843d879d61e9623f1fa6f4312de5e19dbdf0
Tobias Wallura <tobias@wallura.org>
2019-05-05 13:10:05 +0200

    update CanFulfillIntent handling

--- __tests__/CanFulfillRequest.spec.ts
+++ __tests__/CanFulfillRequest.spec.ts
@@ -11,27 +11,44 @@ describe("CanFulfillIntentRequest", () => {
   });
 
   test("Should return YES for valid country", async () => {
-    const result: any = await alexa.filter((request) => {
-      request.request.type = "CanFulfillIntentRequest";
-    }).utter("play the anthem of Germany");
+    const result = await alexa.request()
+      .intent("PlayAnthemIntent")
+      .slot("country", "Germany")
+      .requestType("CanFulfillIntentRequest")
+      .send();
 
     expect(result.response.canFulfillIntent.canFulfill).toBe("YES");
+    expect(result.response.canFulfillIntent.slots.country.canFulfill).toBe("YES");
+    expect(result.response.canFulfillIntent.slots.country.canUnderstand).toBe("YES");
   });
 
   test("Should return NO for invalid country", async () => {
-    const result: any = await alexa.filter((request) => {
-      request.request.type = "CanFulfillIntentRequest";
-    }).intend("PlayAnthemIntent", {
-      country: "InvalidValue",
-    });
+    const result = await alexa.request()
+      .intent("PlayAnthemIntent")
+      .slot("country", "InvalidValue")
+      .requestType("CanFulfillIntentRequest")
+      .send();
 
     expect(result.response.canFulfillIntent.canFulfill).toBe("NO");
+    expect(result.response.canFulfillIntent.slots.country.canFulfill).toBe("NO");
+    expect(result.response.canFulfillIntent.slots.country.canUnderstand).toBe("NO");
+  });
+
+  test("Should return NO if no country is specified", async () => {
+    const result = await alexa.request()
+      .intent("PlayAnthemIntent")
+      .requestType("CanFulfillIntentRequest")
+      .send();
+
+    expect(result.response.canFulfillIntent.canFulfill).toBe("NO");
+    expect(result.response.canFulfillIntent.slots).toBeFalsy();
   });
 
   test("Should return NO for unsupported Intent", async () => {
-    const result: any = await alexa.filter((request) => {
-      request.request.type = "CanFulfillIntentRequest";
-    }).utter("help");
+    const result = await alexa.request()
+      .intent("AMAZON.HelpIntent")
+      .requestType("CanFulfillIntentRequest")
+      .send();
 
     expect(result.response.canFulfillIntent.canFulfill).toBe("NO");
   });
--- src/handlers/CanFulfillIntentRequestHandler.ts
+++ src/handlers/CanFulfillIntentRequestHandler.ts
@@ -7,37 +7,50 @@ import { BaseIntentHandler, getLocale, Request } from "../utils";
 export class CanFulfillIntentRequestHandler extends BaseIntentHandler {
   public async handle(handlerInput: HandlerInput): Promise<Response> {
     return handlerInput.responseBuilder
-      .withCanFulfillIntent({
-        canFulfill: this.isSupported(handlerInput) ? "YES" : "NO",
-      })
+      .withCanFulfillIntent(this.canFulfill(handlerInput))
       .getResponse();
   }
 
-  private isSupported(handlerInput: HandlerInput): boolean {
+  private canFulfill(handlerInput: HandlerInput): canfulfill.CanFulfillIntent {
     const request = handlerInput.requestEnvelope.request as canfulfill.CanFulfillIntentRequest;
     const isIntentSupported = ["PlayAnthemIntent"].indexOf(request.intent.name) !== -1;
-    if (!isIntentSupported) {
-      return false;
+    const hasSlotProvided = request.intent.slots
+      && request.intent.slots.country && request.intent.slots.country.value;
+    if (!isIntentSupported || !hasSlotProvided) {
+      return { canFulfill: "NO" };
     }
 
-    let isSlotSupported = request.intent.slots.country
-      && request.intent.slots.country.resolutions
+    let canUnderstandSlot = request.intent.slots.country.resolutions
       && !!request.intent.slots.country.resolutions.resolutionsPerAuthority
-          .find((val) => val.status.code === "ER_SUCCESS_MATCH");
+        .find((val) => val.status.code === "ER_SUCCESS_MATCH");
+
+    const locale = getLocale(handlerInput) || "en-US" as Locale;
+    const countrySet = countries.getAll(locale)
+      .map((item) => ({
+        item,
+        names: [].concat(...[item.adjectives, item.altNames, [item.name, item.longName]])
+          .filter((val) => !!val)
+          .map((val) => val.toUpperCase()),
+      }));
+
+    // Find country by slot value
+    const country = countrySet.find((item) => item.names.includes(request.intent.slots.country.value.toUpperCase()));
 
     // Compare slot value, if entity resolution did not provide a value
-    if (!isSlotSupported) {
-      const locale: Locale = getLocale(handlerInput) || "en-US";
-      const allPossibleNames: string[] = [].concat(...countries.getAll(locale)
-          .map((item) => [].concat(...[item.adjectives, item.altNames, [item.name, item.longName]])))
-          .filter((item) => !!item)
-          .map((item) => item.toLowerCase());
-      isSlotSupported = request.intent.slots.country
-        && allPossibleNames
-          .filter((item) => item.indexOf(request.intent.slots.country.value.toLowerCase()) !== -1)
-          .length > 0;
+    if (!canUnderstandSlot) {
+      canUnderstandSlot = !!country;
     }
 
-    return isSlotSupported;
+    const canFulfillSlot = country && country.item.anthem.url;
+
+    return {
+      canFulfill: canUnderstandSlot && canFulfillSlot ? "YES" : "NO",
+      slots: {
+        country: {
+          canFulfill: canFulfillSlot ? "YES" : "NO",
+          canUnderstand: canUnderstandSlot ? "YES" : "NO",
+        },
+      },
+    };
   }
 }
